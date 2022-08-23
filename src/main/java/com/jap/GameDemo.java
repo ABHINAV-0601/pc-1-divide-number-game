81105c0f3deac3eca78c4bb5c0402c5e9da412fb
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-23 20:12:21 +0530

    completed and passed the test

--- src/main/java/com/jap/GameDemo.java
+++ src/main/java/com/jap/GameDemo.java
@@ -1,5 +1,7 @@
 package com.jap;
 
+import java.util.Scanner;
+
 public class GameDemo {
 
     public String divideByNumber(int firstNumber, int secondNumber){
@@ -9,11 +11,19 @@ public class GameDemo {
             //write logic
             finalNumber = firstNumber / secondNumber;
             //Handle specific exception
-        }catch (Exception exception){
+        }catch (ArithmeticException exception){
           return exception.toString();
         }
         return String.valueOf(finalNumber);
     }
+    public static void main(String[] args) {
+        GameDemo gameDemo = new GameDemo();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two integer numbers :");
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        System.out.println(gameDemo.divideByNumber(firstNumber, secondNumber));
 
+    }
 
 }
